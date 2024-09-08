a58d9a6aea706f3aee1662b948d84887803525c1
hnhung31 <hnhunght31.10@gmail.com>
2024-09-08 07:48:33 +0700

    Bai 1

---
+++ HelloWorld/src/main/java/vn/iostar/xinChao.java
@@ -0,0 +1,23 @@
+package vn.iostar;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+@WebServlet(urlPatterns = {"/home"})
+public class xinChao extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		PrintWriter printW = response.getWriter();
+		printW.println("<h1>HelloWorld</h1>");
+		printW.close();
+	}
+}
+
