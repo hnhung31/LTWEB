fa13b2ddb5eac42ed03e8da011a22d24e09fc5e2
hnhung31 <hnhunght31.10@gmail.com>
2024-10-08 20:42:06 +0700

    BT JPA tuan 7

---
+++ JPAST4/src/main/java/vn/iostar/configs/JpaConfig.java
@@ -0,0 +1,17 @@
+package vn.iostar.configs;
+
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+import jakarta.persistence.PersistenceContext;
+
+@PersistenceContext
+public class JpaConfig {
+	public static EntityManager getEntityManager() {
+
+		EntityManagerFactory factory = Persistence.createEntityManagerFactory("jpa-hibernate-mysql");
+
+		return factory.createEntityManager();
+
+	}
+}
